55ca9c165f747314c4a1dda7d264840cad2b1410
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-19 03:06:52 +0530

    Added: Player1 position is at 0 at the beginning.

--- src/com/bridgelabz/SnakeAndLadderGame.java
+++ src/com/bridgelabz/SnakeAndLadderGame.java
@@ -1,7 +1,13 @@
+/*
+Snake and Ladder game played with single player at start position 0
+*/
 package com.bridgelabz;
 
 public class SnakeAndLadderGame {
     public static void main(String args[]) {
         System.out.println("Welcome to Snake and Ladder Game. \nLet's Begin.........");
+
+        int playerOnePosition = 0;
+        System.out.println("Player1 position: " + playerOnePosition + "\n");
     }
 }
