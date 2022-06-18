9d72550f600ea20a11d5510410b6b3fb4ab93613
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-19 02:57:02 +0530

    Added: Welcome message.

---
+++ src/com/bridgelabz/SnakeAndLadderGame.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderGame {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Snake and Ladder Game. \nLet's Begin.........");
+    }
+}
