ab4ba15857f52840f64621a7d33814f6ea484b51
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-19 03:26:03 +0530

    Added: Player rolled the die(Used Random).

--- src/com/bridgelabz/SnakeAndLadderGame.java
+++ src/com/bridgelabz/SnakeAndLadderGame.java
@@ -1,13 +1,19 @@
 /*
-Snake and Ladder game played with single player at start position 0
+The Player rolls the die to get a number(1 to 6).
 */
 package com.bridgelabz;
+import java.util.Random; //importing Random function
 
 public class SnakeAndLadderGame {
     public static void main(String args[]) {
         System.out.println("Welcome to Snake and Ladder Game. \nLet's Begin.........");
-
+        //declaring variables
         int playerOnePosition = 0;
+        //Player1 position and die rolled.
         System.out.println("Player1 position: " + playerOnePosition + "\n");
+        System.out.println("Player1 rolls the die.");
+        Random random = new Random();
+        int dieRolled = random.nextInt(6) + 1;
+        System.out.println("Die rolled: " + dieRolled);
     }
 }
